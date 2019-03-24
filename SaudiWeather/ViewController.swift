e9cc3fcf7d2f57051b35182c9f72f58470be6d0f
Alsharif Abdullah M <ammsh92@gmail.com>
2019-03-24 07:49:12 +0300

    Initial Commit

---
+++ SaudiWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SaudiWeather
+//
+//  Created by Alsharif Abdullah M on 3/24/19.
+//  Copyright © 2019 Alsharif Abdullah M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
